a64a3bdb888967bf557af1ff8faa64c68b69c16a
Bright <kapyabright@outlook.com>
2019-09-28 13:06:17 +0200

    initial implementation of private messaging

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/', async (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-var usernames = [];
+var users = {};
 
 // fist thing to happen when a client connects
 io.on('connection', function(socket) {
@@ -24,13 +24,13 @@ io.on('connection', function(socket) {
   socket.on('new user', function(data, callback) {
     // callback because we are sending back data to the code that emitted the event
     // check username array
-    if (usernames.includes(data)) {
+    if (data in users) {
       callback(false);
     } else {
       callback(true);
       // add to array
       socket.username = data;
-      usernames.push(socket.username);
+      users[socket.username] = socket;
 
       updateUsernames();
     }
@@ -38,22 +38,48 @@ io.on('connection', function(socket) {
 
   // emit new usernames
   function updateUsernames() {
-    io.emit('usernames', usernames);
+    io.emit('usernames', Object.keys(users));
   }
 
   socket.on('disconnect', function() {
     console.log('user disconnected');
     // check if user has name
-    if (socket.username) {
+    if (!socket.username) {
       return;
     } else {
-      usernames.splice(usernames.indexOf(socket.username), 1);
+      delete users[socket.username];
       updateUsernames();
     }
   });
-  socket.on('send message', function(data) {
-    // emit data to all users (self included)
-    io.emit('new message', { msg: data, username: socket.username });
+  socket.on('send message', function(data, callback) {
+    // whisper (send private msg)
+    var msg = data.trim();
+    // a whisper msg begins with '/w ' 3 chars
+    if (msg.substring(0, 3) === '/w ') {
+      msg = msg.substring(3);
+      // recepients name follows the whisper syntax and ends at first space
+      var ind = msg.indexOf(' ');
+      if (ind !== -1) {
+        // check if user name is valid
+        var name = msg.substring(0, ind);
+        var msg = msg.substring(ind + 1);
+
+        if (name in users) {
+          users[name].emit('whisper', {
+            msg,
+            username: socket.username
+          });
+        } else {
+          callback('Error, Username not valid');
+        }
+      } else {
+        // no space== no message
+        callback('Error, Please enter a message');
+      }
+    } else {
+      // emit data to all users (self included)
+      io.emit('new message', { msg: data, username: socket.username });
+    }
 
     // send to all except me
     // socket.broadcast.emit('new message', data)
