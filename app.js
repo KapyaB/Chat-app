db59b8017d5bbce034bc9e4e543c673202869241
Bright <kapyabright@outlook.com>
2019-09-28 11:53:12 +0200

    added usernames

--- app.js
+++ app.js
@@ -14,16 +14,46 @@ app.get('/', async (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+var usernames = [];
+
 // fist thing to happen when a client connects
 io.on('connection', function(socket) {
   // socket-the socket that the user is using to connect
   console.log('user connected');
+
+  socket.on('new user', function(data, callback) {
+    // callback because we are sending back data to the code that emitted the event
+    // check username array
+    if (usernames.includes(data)) {
+      callback(false);
+    } else {
+      callback(true);
+      // add to array
+      socket.username = data;
+      usernames.push(socket.username);
+
+      updateUsernames();
+    }
+  });
+
+  // emit new usernames
+  function updateUsernames() {
+    io.emit('usernames', usernames);
+  }
+
   socket.on('disconnect', function() {
     console.log('user disconnected');
+    // check if user has name
+    if (socket.username) {
+      return;
+    } else {
+      usernames.splice(usernames.indexOf(socket.username), 1);
+      updateUsernames();
+    }
   });
   socket.on('send message', function(data) {
     // emit data to all users (self included)
-    io.emit('new message', data);
+    io.emit('new message', { msg: data, username: socket.username });
 
     // send to all except me
     // socket.broadcast.emit('new message', data)
